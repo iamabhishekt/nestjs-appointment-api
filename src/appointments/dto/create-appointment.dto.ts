1975c9d64eeb2ab84ee8c3fba95929e29b1d9c42
Abhishek Thakur <abhishekthakur.art@protonmail.com>
2022-11-27 11:45:40 -0500

    main.ts changes

--- src/appointments/dto/create-appointment.dto.ts
+++ src/appointments/dto/create-appointment.dto.ts
@@ -1,1 +1,65 @@
-export class CreateAppointmentDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateAppointmentDto {
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty()
+  appointmentId: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty()
+  patientId: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty()
+  doctorId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  date: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  time: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  duration: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  location: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  reason: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  notes: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  status: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  created: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  updated: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe(
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    ),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Appointment API')
+    .setDescription(
+      'The API retrieves information about doctor appointments.',
+    )
+    .setExternalDoc('Postman Collection', '/api-json')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
